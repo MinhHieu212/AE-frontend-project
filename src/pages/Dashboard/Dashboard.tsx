40d7b5cfc4252515c2a6f77c58d0d0b270a08461
MinhHieu212 <hieu.tranminhk21@hcmut.edu.vn>
2024-09-04 00:19:42 +0700

    nothing change, just check github status

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ import PopularProducts from "./components/PopularProducts";
 
 const Dashboard = () => {
   return (
-    <div className="w-full max-w-[1430px] py-[50px] mx-auto bg-white">
+    <div className="w-full max-w-[1430px] py-[40px] mx-auto bg-white">
       <BannerCarousel />
       <PopularProducts />
     </div>
